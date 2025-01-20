6156e5a394bf617949003ded62d1f7ee7ea32e62
GeethapriyanS <geethapriyan.s2023cse@sece.ac.in>
2025-01-20 12:57:46 +0530

    class components are added

---
+++ seceFrontend/src/components/classComponents/ClassCompEG.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+class ClassCompEG extends React.Component{
+    render(){
+      return(
+        <div>
+           <h3>This is class components example</h3>
+        </div>   
+      )
+    }
+}
+
+export default ClassCompEG;
---
+++ seceFrontend/src/components/classComponents/Gallery.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+class Gallery extends React.Component{
+    render(){
+        return(
+            <div>
+                <h3>Gallery Component</h3>
+            </div>
+        )
+    }
+}
+export default Gallery;
